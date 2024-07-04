2f78de7a84fadeb0e7b71ece7de9da597927994e
levikaw <levkin_dmitrii@mail.ru>
2024-07-04 21:12:50 +0500

    feat(docker): dockerfile

--- src/modules/user/domain/utils.ts
+++ src/modules/user/domain/utils.ts
@@ -1,4 +1,4 @@
-import { compare, genSalt, hash } from 'bcrypt';
+import { compare, genSalt, hash } from 'bcryptjs';
 
 export const generateHash = async (password: string): Promise<string> =>
   genSalt(10).then((salt: string) => hash(password, salt));
