9b0a67a3790e46d4212de4d5917207c816296d59
levikaw <levkin_dmitrii@mail.ru>
2024-07-04 11:30:58 +0500

    fix: move to constants

---
+++ src/common/constants.ts
@@ -0,0 +1,10 @@
+export const EXTEPTIONS = {
+  USER_NOT_FOUND: 'User not found',
+  TASKS_NOT_FOUND: 'Tasks not found',
+  WRONG_PASSWORD: 'Incorrect password',
+  AUTH_ERROR: 'Unable to auth this user',
+  CREATE_TASK_ERROR: 'Unable to create task',
+  CREATE_USER_ERROR: 'Unable to create user',
+} as const;
+
+export const MIN_LENGTH_PASSWORD = 5;
